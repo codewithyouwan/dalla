83ee6d5006fda59c550a46f5a932dfb73ccc1a46
codewithyouwan <101477954+codewithyouwan@users.noreply.github.com>
2025-06-24 11:02:16 +0900

    Fix: Now the preview and the save api's are different both are not triggered at once.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/temp/:path*',
+        destination: '/api/serveTemp?path=:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
