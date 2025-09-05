2848eb4a46c848e8a78bf396a18765742379bac8
codewithyouwan <101477954+codewithyouwan@users.noreply.github.com>
2025-09-05 11:01:22 +0900

    Feature: Made components of the delete and undo buttons.

--- frontend/src/app/components/resume/PersonalInfo.jsx
+++ frontend/src/app/components/resume/PersonalInfo.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useRef } from 'react';
 import Cropper from 'react-easy-crop';
 import { getCroppedImg } from '../../helper/ImageCrop/cropUtils';
+import DeleteButton from '../buttons/DeleteButton';
+import UndoButton from '../buttons/UndoButton';
 import { set } from 'date-fns';
 
 export default function PersonalInfo({ 
@@ -149,26 +151,17 @@ export default function PersonalInfo({
             className="mt-1 block text-black w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
             placeholder="例: 読書"
           />
-          <div className='absolute top-0 right-0 flex'>
-            <button
-            onClick={() => setDetails(prev => ({...prev, hobby: prevDetails.hobby}))}
-            className="text-blue-600 hover:text-blue-800 p-1"
-            title="Undo changes"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
-                clipRule="evenodd"
+          {prevDetails.hobby!==''&&prevDetails.hobby!==details.hobby&&
+            <div className='absolute top-0 right-0 flex'>
+              <UndoButton
+                clickFunction={() =>{
+                    setDetails(prev => ({...prev, hobby: prevDetails.hobby}));
+                    setPrevDetails(prev=>({...prev,hobby:''}));
+                  }
+                }
               />
-            </svg>
-          </button>
-          </div>
+            </div>
+          }
         </div>
         {newDetails.hobby!==''&&
           (
@@ -187,25 +180,12 @@ export default function PersonalInfo({
                 </li>
               </ul>
               <div className="absolute top-0 right-0 flex">
-                <button
-                  onClick={() => setNewDetails((prev) => ({ ...prev, hobby: '' }))}
-                  className="text-red-600 hover:text-red-800 p-1"
-                  title="Delete this new hobby suggestion"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                      clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
+                <DeleteButton
+                  clickFunction = {
+                    ()=> setNewDetails((prev)=>({...prev,hobby:''}))
+                  }
+                />
+              </div>
             </div>
           )
         }
