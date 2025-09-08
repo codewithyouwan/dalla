7b27c03602a10f199896e5442dac3c34366d1012
codewithyouwan <101477954+codewithyouwan@users.noreply.github.com>
2025-09-08 15:36:09 +0900

    Update: Disabling sharp library.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.resolve.extensions = [
       ...config.resolve.extensions,
