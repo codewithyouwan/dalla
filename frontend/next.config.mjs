9f4537688105de86f0748a562c1788e459759f6b
codewithyouwan <101477954+codewithyouwan@users.noreply.github.com>
2025-09-08 14:47:35 +0900

    Fix: Updated the extensions in the next.config.mjs file. It was causing the errors.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.extensions = ['.js', '.mjs', '.json', '.cjs'];
+    config.resolve.extensions = [
+      ...config.resolve.extensions,
+      '.js',
+      '.mjs',
+      '.json',
+      '.cjs',
+      '.jsx',
+      '.ts',
+      '.tsx',
+    ];
     return config;
   },
 };
